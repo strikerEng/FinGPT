5628f0c3e4cf229e066e48cf2409c6fa47646202
strikerEng <kyrheepowell@gmail.com>
2024-07-17 19:54:37 -0700

    Updated the requirements file and added a new folder with commonly used constants.

---
+++ fingpt/FinGPT_Constants/constants.py
@@ -0,0 +1,5 @@
+SUPPORTED_BASE_MODELS = {
+    'Llama2': 'meta-llama/Llama-2-7b-chat-hf',
+    'Llama3_Instruct': 'meta-llama/Meta-Llama-3-8B-Instruct',
+    'Llama3': 'meta-llama/Meta-Llama-3-8B'
+}
